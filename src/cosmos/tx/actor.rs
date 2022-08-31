0a7ee704d23fb3e623486296a2c113247d613214
Benjamin DENEUX <b.deneux@okp4.com>
2022-08-31 08:58:03 +0200

    refactor: trigger transaction interval

--- src/cosmos/tx/actor.rs
+++ src/cosmos/tx/actor.rs
@@ -1,6 +1,16 @@
+use std::time::Duration;
 use crate::cosmos::tx::TxHandler;
-use actix::{Actor, Context};
+use actix::{Actor, AsyncContext, Context};
+use tracing::info;
+use crate::cosmos::tx::messages::trigger::TriggerTx;
 
 impl Actor for TxHandler {
     type Context = Context<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        info!("📣 TxHandler started...");
+        ctx.run_interval(Duration::new(8, 0), |_, ctx| {
+            ctx.address().do_send(TriggerTx { memo: "test".to_string(), gas_limit: 200000 })
+        });
+    }
 }
