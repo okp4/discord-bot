a52ef4ff199f91eeb3fe73c306a647cc720f3666
Benjamin DENEUX <b.deneux@okp4.com>
2022-09-27 09:00:52 +0200

    feat: handle tx response on faucet to send discord message

--- src/cosmos/faucet/handlers/tx_response.rs
+++ src/cosmos/faucet/handlers/tx_response.rs
@@ -2,11 +2,16 @@ use crate::cosmos::faucet::Faucet;
 use crate::cosmos::tx::messages::response::{TxResponse, TxResponseResult};
 use actix::Handler;
 use tracing::info;
+use crate::cosmos::faucet::discord_message::FaucetTransactionMessage;
+use crate::cosmos::tx::discord_message::TransactionDiscordMessage;
+use crate::discord::discord_client::messages::send_msg::SendMessage;
 
 impl Handler<TxResponse> for Faucet {
     type Result = TxResponseResult;
 
-    fn handle(&mut self, _: TxResponse, _: &mut Self::Context) -> Self::Result {
-        info!("✍️  📝 Receive transaction result on faucet");
+    fn handle(&mut self, msg: TxResponse, _: &mut Self::Context) -> Self::Result {
+        info!("📝 Receive transaction result on faucet.");
+        let message = FaucetTransactionMessage::build_message(msg.response, msg.subscribers, self.channel_id);
+        self.discord_client.do_send(SendMessage { message });
     }
 }
