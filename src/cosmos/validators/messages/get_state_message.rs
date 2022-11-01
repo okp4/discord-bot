4c24efbe4530e22346c35b677f84b3d8acae984a
Adrien Touzouli <a.touzouli@okp4.com>
2022-11-01 17:24:54 +0100

    feat(validator): manage validators request pagination

--- src/cosmos/validators/messages/get_state_message.rs
+++ src/cosmos/validators/messages/get_state_message.rs
@@ -1,8 +1,15 @@
 use actix::Message;
+use cosmos_sdk_proto::cosmos::staking::v1beta1::BondStatus;
 
 pub type GetStateResult = ();
 
 /// Empty message
 #[derive(Message)]
 #[rtype(result = "GetStateResult")]
-pub struct GetStateMessage {}
+pub struct GetStateMessage {
+    /// Bond status
+    pub bond_status: BondStatus,
+
+    /// Pagination key
+    pub pagination_key: Option<Vec<u8>>,
+}
